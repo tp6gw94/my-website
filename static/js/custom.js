8779ce63ab18f4deae876bbc5ebf54833fbd8f2a
tp6gw94 <tp6gw94@gmail.com>
2021-06-18 21:27:09 +0800

    [chore] update prism & add mermaid

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,7 +2,6 @@ document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
     // document ready
     pangu.spacingElementById('main');
-    console.log(123);
 
     document.addEventListener('DOMContentLoaded', () => {
       // listen to any DOM change and automatically perform spacing via MutationObserver()
